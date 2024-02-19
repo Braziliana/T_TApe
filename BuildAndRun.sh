b405437176d65bff58552c215f54a196a9fc81ec
Koelion <koelionekpl@gmail.com>
2024-02-19 14:20:53 +0100

    Build and run script

--- BuildAndRun.sh
+++ BuildAndRun.sh
@@ -12,13 +12,13 @@ cmake ..
 
 make
 
-if [ -f T_TApe ]; then
+if [ -f Apex ]; then
   # filename=$(openssl rand -hex $((RANDOM % 11 + 10)))
   # mv T_TApe $filename
   # sudo chmod 700 $filename
   # sudo ./$filename
   # rm $filename
 
-  sudo chmod 700 T_TApe
-  sudo ./T_TApe
+  sudo chmod 700 Apex
+  sudo ./Apex
 fi
